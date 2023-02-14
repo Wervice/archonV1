97549d441651a7d38c2bc008e448c52b93a7a1e8
wervice <108389772+Wervice@users.noreply.github.com>
2023-02-14 15:17:28 +0100

    Add files via upload

--- archon/__main__.py
+++ archon/__main__.py
@@ -3,6 +3,7 @@ import sys
 from libs import dashboard as dashboard
 import shutil
 import re
+import ftplib
 from termcolor import colored
 from datetime import datetime
 now = datetime.now()
@@ -16,7 +17,7 @@ try:
         # Writes the breefing
         print(open(".\\assets\\breefing.txt", "r").read())
     else:
-        if sys.argv[1] == "backup":
+        if sys.argv[1] == "sbackup":
             # Backup a single file
             try:
                 # Copies the file to the destination location
@@ -60,14 +61,14 @@ try:
                 os.remove(sys.argv[2]+"archon_metadata")
                 print(colored("[*] Backup folder", "green"))  # Print Done
                 # Push to the dashboard
-                dashboard.push("success", sys.argv[2], sys.argv[3], True)
+                dashboard.push("success", sys.argv[2], sys.argv[3]+foldername+".tar.xz", True)
                 dashboard.show()
             except FileNotFoundError:
                 dashboard.push("fail_filenotfound",
-                               sys.argv[2], sys.argv[3], True)
+                               sys.argv[2], sys.argv[3]+foldername+".tar.xz", True)
                 dashboard.show()
                 print(colored("File Not Found\nExiting now", "red"))
-        elif sys.argv[1] == "restore":
+        elif sys.argv[1] == "frestore":
             try:
                 print(colored("[-] Getting backups", "blue"))  # Status message
                 # Download the backup
@@ -93,6 +94,117 @@ try:
                     metadata.split(";\n")[0]+")", "green"))
             except FileNotFoundError:
                 print(colored("File Not Found\nExiting now", "red"))
+        elif sys.argv[1] == "fbackupftp":
+            host = input(colored("Host: ", "blue"))
+            username = input(colored("Username: ", "blue"))
+            password = input(colored("Password: ", "blue"))
+            backup_archive = ".\\archon_temp_backup_ftp_archive.tar.xz"
+            backup_location = sys.argv[3]
 
+            try:
+                print(colored("[-] Generate archive", "blue"))
+                foldername = os.path.dirname(sys.argv[2]).split(
+                    "\\")[len(os.path.dirname(sys.argv[2]).split("\\"))-1]  # Get foldername
+                print(colored("[-] Make compressed tar archive", "blue"))  # Print "Make the archive"
+                # Write the metadata to the backupfolder
+                open(sys.argv[2]+"archon_metadata",
+                     "w").write(dt_string+";\n"+str(now)+";\n"+os.getlogin())
+                # Make the arcive
+                shutil.make_archive(
+                    ".\\archon_temp_backup_ftp_archive", 'xztar', sys.argv[2])
+                # Delete the metadata from the folder
+                os.remove(sys.argv[2]+"archon_metadata")
+                print(colored("[*] Backup folder", "green"))  # Print Done
+            except FileNotFoundError:
+                print(colored("File Not Found\nExiting now", "red"))
+                
+            print(colored("[-] Connecting", "blue"))
+            if sys.argv[4] == "tls":
+                print(colored("[-] Will use TLS (FTPS)", "blue"))
+                connection = ftplib.FTP_TLS(host=host)
+            elif sys.argv[4] == "none":
+                print(colored("[?] Your connection is not secure. Do you wan to continue?", "red"))
+                if input("Y/n> ") == "Y":
+                    connection = ftplib.FTP(host=host)
+                else:
+                    print(colored("[x] Abborted", "red"))
+                    exit()
+            else:
+                print(colored("[?] Your connection is not secure. Do you wan to continue?", "red"))
+                if input("Y/n> ") == "Y":
+                    connection = ftplib.FTP(host=host)
+                else:
+                    print(colored("[x] Abborted", "red"))
+                    exit()
+            connection.connect()
+            try:
+                connection.login(user=username, passwd=password)
+                print(colored("[*] Connected", "green"))
+            except ftplib.error_perm:
+                print(colored("[x] Permission Error", "red"))
+                exit()
+            try:
+                connection.storbinary("STOR %s" % (backup_location),  open(backup_archive, "rb"))
+                print(colored("[*] Wrote file to server", "green"))
+            except ftplib.error_perm:
+                print(colored("[x] You're not allowed to write the backup there", "red"))
+            connection.close()
+            os.remove(".\\archon_temp_backup_ftp_archive.tar.xz")
+            print(colored("[-] FTP Connection Closed", "blue"))
+            print(colored("[*] Done", "green"))
+        if sys.argv[1] == "frestoreftp":
+            host = input(colored("Host: ", "blue"))
+            username = input(colored("Username: ", "blue"))
+            password = input(colored("Password: ", "blue"))
+
+            # FTP
+            # Build FTP Connection
+            if sys.argv[4] == "tls":
+                print(colored("[-] Will use TLS (FTPS)", "blue"))
+                connection = ftplib.FTP_TLS(host=host)
+            elif sys.argv[4] == "none":
+                print(colored("[?] Your connection is not secure. Do you wan to continue?", "red"))
+                if input("Y/n> ") == "Y":
+                    connection = ftplib.FTP(host=host)
+                else:
+                    print(colored("[x] Abborted", "red"))
+                    exit()
+            else:
+                print(colored("[?] Your connection is not secure. Do you wan to continue?", "red"))
+                if input("Y/n> ") == "Y":
+                    connection = ftplib.FTP(host=host)
+                else:
+                    print(colored("[x] Abborted", "red"))
+                    exit()
+            connection.login(user=username, passwd=password)
+            print(colored("[*] Connected", "green"))
+            connection.retrbinary("RETR %s" % (sys.argv[2]), open("temp_archon_restore_file_ftp.tmp", "wb").write)
+        
+            try:
+                print(colored("[-] Getting backups", "blue"))  # Status message
+                # Download the backup
+                if os.path.exists(".\\temp_archon_restore_file_ftp.tmp"):
+                    print(colored("[*] Got backups", "green"))
+                else:
+                    print(colored("[x] Getting backups failed. Please try again.", "red"))
+                    print("Exiting now")
+                    exit()
+                print(colored("[-] Restoring files", "blue"))  # Status message
+                # Unpack the downloaded archive
+                shutil.unpack_archive(
+                    ".\\temp_archon_restore_file_ftp.tmp", sys.argv[3], format="xztar")
+                print(colored("[*] Restoring files", "green"))
+                # Read the metadata to the chache
+                metadata = open(sys.argv[3]+"archon_metadata", "r").read()
+                print(colored("[-] Remove temporary files", "blue"))  # Status message
+                os.remove(sys.argv[3]+"archon_metadata")  # Remove metadata file
+                os.remove(".\\temp_archon_restore_file_ftp.tmp") # Remove downloaded file
+                # Print status message and metadata time
+                print(colored("[*] Restored from backup (Backup Time " +
+                    metadata.split(";\n")[0]+")", "green"))
+            except FileNotFoundError:
+                print(colored("File Not Found\nExiting now", "red"))
+            except ftplib.error_perm:
+                print(colored("Permission Error. Please check your password and username.", "red"))
 except IndexError:
     print(colored("You need more arguments.\nExiting now", "red"))
--- archon/libs/dashboard.py
+++ archon/libs/dashboard.py
@@ -12,9 +12,9 @@ def get_file_extension(file_path):
 
 def push(status, file, to, folder=False):
     if is_unc_path(to):
-        note = to + " is a network resource."
+        note = to + " is a network resource. You should unmount it after backup."
     else:
-        note = to + " is a local path. If it's an removable drive, make sure it's disconected from your PC after backup."
+        note = to + " seemes to be a local path. If it's a removable drive, make sure it's disconected from your PC after backup."
     icon = "mimeicons/mimeicon_"+get_file_extension(file)+".svg"
     open("assets/backup_list.html", "a").write("<tr><td>"+file+" to "+to+"</td><td>"+status.replace("success", "<span class=green>Success</span>").replace("fail_copyisfalse", "<span class=red>Copy is not correct</span>").replace("fail_filenotfound", "<span class=red>File Not Found</span>")+"</td><td>"+dt_string+"</td></tr>")
     open("assets/backup_list.csv", "a").write("\n"+file+" -> "+to+";"+status+";"+dt_string+";;")
